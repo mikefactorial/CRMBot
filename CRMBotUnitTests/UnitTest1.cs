0e643bb151a9b55094973203704a36c42b6726cf
micha <micha@DESKTOP-RQBMUQ5>
2016-05-17 16:02:47 -0400

    Test Updates

--- CRMBotUnitTests/UnitTest1.cs
+++ CRMBotUnitTests/UnitTest1.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Bot.Connector;
+using CRMBot;
+using System.Threading.Tasks;
 
 namespace CRMBotUnitTests
 {
     [TestClass]
     public class UnitTest1
     {
+        public static string conversationId = Guid.NewGuid().ToString();
+
         [TestMethod]
         public void TestLocateIntent()
         {
+            MessagesController controller = new MessagesController();
+            Message message = new Message();
+            message.Type = "Message";
+            message.Text = "Find contact Susan Jones."; 
+            message.ConversationId = conversationId;
 
+            Task<Message> responseMessage = controller.Post(message);
+            Assert.IsNotNull(responseMessage.Result);
+            Assert.IsTrue(responseMessage.Result.Text.Contains("I found a"));
         }
     }
 }
