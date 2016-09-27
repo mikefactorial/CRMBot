309fa4f0da6f4bfec076a6a48f97e4a467116586
micha <micha@DESKTOP-RQBMUQ5>
2016-09-26 22:59:53 -0400

    Welcome form updates

--- CRMBot/Forms/LeadForm.cs
+++ CRMBot/Forms/LeadForm.cs
@@ -23,14 +23,12 @@ namespace CRMBot.Forms
                 using (OrganizationServiceProxy serviceProxy = CrmHelper.CreateOrganizationService(Guid.Empty.ToString()))
                 {
                     Microsoft.Xrm.Sdk.Entity newLead = new Microsoft.Xrm.Sdk.Entity("lead");
-                    Guid leadId = Guid.NewGuid();
-                    newLead["leadid"] = leadId;
                     newLead["subject"] = "CRM Bot Registrant";
                     if (nameSplit.Length > 0) newLead["firstname"] = nameSplit[0];
                     if (nameSplit.Length > 1) newLead["lastname"] = nameSplit[1];
                     newLead["cobalt_leadtype"] = new OptionSetValue() { Value = (state.Type == LeadType.Partner) ? 533470001 : 533470000 };
                     serviceProxy.Create(newLead);
-                    await context.PostAsync($"Thanks for saying Hi {nameSplit[0]}! If you’d like to connect me to your CRM Organization go [here](http://www.cobalt.net/botregistration?id={leadId}).");
+                    await context.PostAsync($"Thanks for saying Hi {nameSplit[0]}! If you’d like to connect me to your CRM Organization go [here](http://www.cobalt.net/botregistration).");
                 }
             };
             return new FormBuilder<LeadForm>()
